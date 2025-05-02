89a0f8bff2545ea206a65fbda7db165ede0cca15
andres <andresdavid.ortega@gmail.com>
2025-05-02 14:54:17 -0500

    se arreglo return in GenericPage

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,7 @@ import Prubas from "./pages/home/Prubas";
 import PageCancerLiver from "./pages/liver/desease/PageCancerLiver";
 import ScrollToTop from "./help/ScrollToTop";
 import PageSymptomsFever from "./pages/liver/symptoms/PageSymptomsFever";
-import PageModel from "./models-3d/liver_cancer/common/pages/PageModel";
-import ModelCancerInPage from "./models-3d/liver_cancer/common/ModelCancerInPage";
-import CancerText3D from "./models-3d/liver_cancer/common/texts/CancerText3D";
-import { LiverCancerFinal } from "./models-3d/liver_cancer/desease/cancer/LiverCancerFinal";
+import GenericPage from "./GenericPage";
 
 function App() {
   return (
@@ -22,29 +19,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/prubas" element={<Prubas />} />
-          <Route path="/cancerLiver" element={<PageCancerLiver />} />
-          <Route path="/symptoms" element={<PageSymptomsFever />} />
-          <Route
-            path="/genericpage"
-            element={
-              <PageModel
-                modelpage={
-                  <ModelCancerInPage
-                    text={
-                      <CancerText3D
-                        title={"LIVER CANCER"}
-                        position={[0, 1.5, 0]}
-                        color="red"
-                      />
-                    }
-                    model={<LiverCancerFinal scale={2} home={false} />}
-                    color="#ff6035"
-                  />
-                }
-                color="#ff6035"
-              />
-            }
-          />
+          {/* <Route path="/cancerLiver" element={<PageCancerLiver />} /> */}
+          {/* <Route path="/symptoms" element={<PageSymptomsFever />} /> */}
+          <Route path="/cancer" element={<GenericPage page="cancer" />} />
+          <Route path="/symptoms" element={<GenericPage page="symptoms" />} />
         </Routes>
       </Layout>
     </BrowserRouter>
